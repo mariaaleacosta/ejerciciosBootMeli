fb889c91996a8fde9c25f93b6d39dd26ce4ee061
mariaaleacosta <maria.acosta@mercadolibre.com.co>
2021-09-16 13:20:47 -0500

    coments en el codigo

--- chimpokomon/pokemon.js
+++ chimpokomon/pokemon.js
@@ -26,26 +26,21 @@ getPokemons();
 
 //obtener 15 pokemons  y sacar los quue pesen mas de 150
 async function getPokemonsWeight(){
-    let pokemon = await fetch(`${baseURL}/pokemon?offset=100&limit=15`);
-    let pokemons= await (pokemon.json());
-     const arrayPokemon= await (Promise.all(pokemons.results.map(res=> fetch(res.url))));
 
+    // consumir la api 
+    let pokemon = await fetch(`${baseURL}/pokemon?offset=100&limit=15`);
+
+       // convierto la data a json 
+    let pokemons= await (pokemon.json());
+
+    //hago un nuevo fetch para traerme la url que esta dentro de  los resultados de pokemon  
+     const arrayPokemon= await pokemons.results.map(res=> fetch(res.url));
+
+     //recorro el array de chimpokomons de nuevo para sacar el  peso del chimpokomon
      const pokemonWeight= await Promise.all(arrayPokemon.map(pokeW=> pokeW.json()));
 
      const filterPrueba= pokemonWeight.filter(data=> data.weight>=150);
      console.log(filterPrueba);
-
-   /*  console.log(results) */
-
-/*     console.log(pokemons); */
- 
-
-    
-
-    
-    
 }
-
-
 getPokemonsWeight();
  
